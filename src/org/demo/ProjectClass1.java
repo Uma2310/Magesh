c0dbc6f8db9caa6146b13702f4f5aafd79abee4e
Uma2310 <umamageshselva@gmail.com>
2025-07-30 13:33:23 +0530

    project completed

---
+++ src/org/demo/ProjectClass1.java
@@ -0,0 +1,20 @@
+package org.demo;
+
+
+
+public class ProjectClass1 {
+private void sample() {
+	System.out.println("Project 1");
+
+}
+private void sample2() {
+	System.out.println("Project 2");
+
+}
+public static void main(String[] args) {
+	ProjectClass1 pc=new ProjectClass1();
+	
+	pc.sample();
+	pc.sample2();
+}
+}
