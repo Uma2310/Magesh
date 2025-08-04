1bf06bbf977721f75d117e5b210bbe8850cea2f7
Uma2310 <umamageshselva@gmail.com>
2025-08-04 18:00:53 +0530

    gitprojectm

--- src/org/demo/ProjectClass1.java
+++ src/org/demo/ProjectClass1.java
@@ -11,10 +11,16 @@ private void sample2() {
 	System.out.println("Project 2");
 
 }
+private void git() {
+	System.out.println("project 3");
+
+}
 public static void main(String[] args) {
 	ProjectClass1 pc=new ProjectClass1();
 	
 	pc.sample();
 	pc.sample2();
+	pc.git();
+	pc.git();
 }
 }
